67dcb7991e1ff54b0a816de761b8908e2a20a6c4
viiirus07 <viiirus07@gmail.com>
2021-05-15 16:04:52 +0530

    Database connection

--- setup.js
+++ setup.js
@@ -1,20 +1,40 @@
 document.getElementById('setup-btn').addEventListener('click', setup());
-let git;
-let stack;
-let gplus;
-let fb;
-let insta;
-let linkedin;
+
 
 function setup() {
-    git = document.getElementById('github').value;
-    stack = document.getElementById('stack').value;
-    gplus = document.getElementById('gmail').value;
-    fb = document.getElementById('facebook').value;
-    insta = document.getElementById('insta').value;
-    linkedin = document.getElementById('linkedin').value;
+    var git = document.getElementById('github').value;
+    var stack = document.getElementById('stack').value;
+    var gplus = document.getElementById('gmail').value;
+    var fb = document.getElementById('facebook').value;
+    var insta = document.getElementById('insta').value;
+    var linkedin = document.getElementById('linkedin').value;
 
-    document.getElementById('github-link').setAttribute('href', git);
+    /* document.getElementById('github-link').setAttribute('href', git);
     document.getElementById('stack-link').setAttribute('href', stack);
+ */
+    addData();
+}
 
+function addData() {
+    const dbname = "Database";
+    const requestDB = window.indexedDB.open(dbname);
+    requestDB.onupgradeneeded = () => {
+        var db = requestDB.result;
+        var store = db.createObjectStore('links', { autoIncrement: true });
+        store.put({
+            title: "ABC",
+            author: "SSSS"
+
+        });
+        store.put({
+            title: "EFG",
+            author: "XXXXXX"
+        });
+    }
+
+
+    requestDB.onsuccess = () => {
+        if (requestDB.readyState == "done")
+            console.log("DATA UPLOADED");
+    }
 }

edebdab5b88115532ddf2fbe0eb800a02afef692
viiirus07 <viiirus07@gmail.com>
2021-05-21 02:21:52 +0530

    dynamic href

--- popup.js
+++ popup.js
@@ -2,5 +2,19 @@ window.onload = function() {
     chrome.storage.sync.get('git', function(data) {
         document.getElementById('github-link').setAttribute('href', data.git);
     });
-
+    chrome.storage.sync.get('stack', function(data) {
+        document.getElementById('stack-link').setAttribute('href', data.stack);
+    });
+    chrome.storage.sync.get('gplus', function(data) {
+        document.getElementById('g-link').setAttribute('href', data.gplus);
+    });
+    chrome.storage.sync.get('fb', function(data) {
+        document.getElementById('fb-link').setAttribute('href', data.fb);
+    });
+    chrome.storage.sync.get('insta', function(data) {
+        document.getElementById('insta-link').setAttribute('href', data.insta);
+    });
+    chrome.storage.sync.get('linkedin', function(data) {
+        document.getElementById('linkedin-link').setAttribute('href', data.linkedin);
+    });
 }
--- setup.js
+++ setup.js
@@ -1,23 +1,21 @@
 document.getElementById('setup-btn').addEventListener('click', function() {
     var git = document.getElementById('github').value;
-
     chrome.storage.sync.set({ "git": git });
+
+    var stack = document.getElementById('stack').value;
+    chrome.storage.sync.set({ "stack": stack });
+
+    var gplus = document.getElementById('gmail').value;
+    chrome.storage.sync.set({ "gplus": gplus });
+
+    var fb = document.getElementById('facebook').value;
+    chrome.storage.sync.set({ "fb": fb });
+
+    var insta = document.getElementById('insta').value;
+    chrome.storage.sync.set({ "insta": insta });
+
+    var linkedin = document.getElementById('linkedin').value;
+    chrome.storage.sync.set({ "linkedin": linkedin });
+
+    alert("Uploaded");
 });
-
-
-
-
-
-
-
-
-/* var stack = document.getElementById('stack').value;
-var gplus = document.getElementById('gmail').value;
-var fb = document.getElementById('facebook').value;
-var insta = document.getElementById('insta').value;
-var linkedin = document.getElementById('linkedin').value;
-
-chrome.storage.sync.set({ "git": git }); */
-/* document.getElementById('github-link').setAttribute('href', git);
-    document.getElementById('stack-l-ink').setAttribute('href', stack);
- */
